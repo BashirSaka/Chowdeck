73418387a07f83037796c95495fc70a9dafef40e
Sultan <Bashirola2008@gmail.com>
2026-09-18 11:31:55 +0100

    Updated chowdeck

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,20 +1,20 @@
 import { Play, MessageSquare, Apple } from "lucide-react";
 import Rider from "./Rider";
-import Imagechow from "../../assets/bg-image.png";
+import Imagechow from "../../assets/bg-image.png"; 
 
 export default function Hero() {
   return (
-    <section className="relative flex flex-col items-center pb-32 min-h-[70vh] md:min-h-[60vh] md:overflow-hidden">
+    <section className="relative flex flex-col items-center  min-h-[70vh] md:min-h-[60vh]  md:overflow-hidden">
       <img
         src={Imagechow}
-        className="absolute inset-0 w-full h-full object-cover -z-10"
-        alt=""
+        className="h-screen md:w-full h-full object-cover -z-10"
+        alt="Image of a city view"
       />
-      <h1 className="font-bold text-center text-black text-6xl  md:mt-20 absolute">
+      <h1 className="font-bold text-center text-black text-5xl md:text-6xl mt-10 md:mt-20 absolute">
         Se o ti jeun?
       </h1>
 
-      <div className="flex gap-4 justify-center items-center mt-40 px-4 md:mt-46 absolute">
+      <div className="flex gap-4 justify-center items-center mt-32 px-4 md:mt-46 absolute">
         <p className="bg-green-800 rounded-md p-4 flex flex-wrap  items-center gap-2 text-white cursor-pointer hover:bg-green-900 transition-colors">
           <Play size={25} className="text-green-400" />
           Download on Google Play
@@ -25,14 +25,14 @@ export default function Hero() {
         </p>
       </div>
 
-      <div className="mt-48">
+      <div>
         {/* Bottom-left Delivery Rider Icon (facing left) */}
-        <div className="absolute bottom-0  left-8 md:bottom-10 md:left-24 lg:left-32 cursor-pointer">
+        <div className="absolute bottom-2 left-8 md:left-24 lg:left-32 cursor-pointer">
           <Rider />
         </div>
 
         {/* Right-side Support / Message Icon — positioned above the rider */}
-        <div className="absolute right-4 md:right-8 bottom-24 md:bottom-52">
+        <div className="absolute right-4 md:right-8 bottom-44 md:bottom-52">
           <button
             className="bg-[#033b2c] text-white p-3.5 rounded-2xl shadow-lg hover:scale-105 transition-transform flex items-center justify-center cursor-pointer"
             aria-label="Open Intercom Messenger"
